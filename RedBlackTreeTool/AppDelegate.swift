d556b2281f7883dfaebdd1217c114f40483cb88a
Erik Spooner <Spoons376@icloud.com>
2022-02-11 13:44:25 -0700

    Initial commit

---
+++ RedBlackTreeTool/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  RedBlackTreeTool
+//
+//  Created by Erik Spooner on 2022-02-11.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ RedBlackTreeTool/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  RedBlackTreeTool
+//
+//  Created by Erik Spooner on 2022-02-11.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
