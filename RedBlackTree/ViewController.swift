5a83bce66c04e166ed789b876f8f34436cc23d8e
Erik Spooner <Spoons376@icloud.com>
2022-09-07 14:37:48 -0600

    Initial Commit

---
+++ RedBlackTree/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RedBlackTree
+//
+//  Created by Erik Spooner on 2022-09-07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
